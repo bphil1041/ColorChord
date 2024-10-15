0af6233a4a00316f4809d8b791afabc0386bdae3
bphil1041 <benjamin.r.phillips1041@gmail.com>
2024-10-14 20:03:26 -0500

    Update SoundManager.js

--- colorchord/src/components/SoundManager.js
+++ colorchord/src/components/SoundManager.js
@@ -10,10 +10,17 @@ const startSoundForColor = (color) => {
     // Map color to a specific note or sound frequency
     const colorToNote = {
         red: 'C4',
-        blue: 'D4',
-        green: 'E4',
-        yellow: 'F4',
-        purple: 'G4',
+        orangered: 'C#4',
+        orange: 'D4',
+        darkorange: 'D#4',
+        yellow: 'E4',
+        yellowgreen: 'F4',
+        green: 'F#4',
+        cyan: 'G4',
+        blue: 'G#4',
+        indigo: 'A4',
+        purple: 'A#4',
+        magenta: 'B4'
     };
 
     const note = colorToNote[color] || 'C4'; // Default to C4 if color isn't mapped
