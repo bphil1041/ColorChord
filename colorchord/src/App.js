0767a786b125bb3e76a8421cd5d27842bc034fe1
bphil1041 <benjamin.r.phillips1041@gmail.com>
2024-10-15 00:33:29 -0500

    Update App.js

--- colorchord/src/App.js
+++ colorchord/src/App.js
@@ -4,11 +4,27 @@ import Toolbar from '../src/components/Toolbar';
 
 const App = () => {
   const [selectedColor, setSelectedColor] = useState('black'); // Default color
+  const [selectedBrushSize, setSelectedBrushSize] = useState(5); // Default brush size
+
+  // Define the handleStrokeEnd function
+  const handleStrokeEnd = () => {
+    console.log('Stroke ended!');
+    // You can add any additional logic when the drawing stroke ends
+  };
 
   return (
     <div>
-      <Toolbar setSelectedColor={setSelectedColor} />
-      <DrawingCanvas selectedColor={selectedColor} onStrokeEnd={() => { }} />
+      <Toolbar
+        setSelectedColor={setSelectedColor}
+        selectedColor={selectedColor}
+        setSelectedBrushSize={setSelectedBrushSize}
+        selectedBrushSize={selectedBrushSize}
+      />
+      <DrawingCanvas
+        selectedColor={selectedColor}
+        selectedBrushSize={selectedBrushSize}
+        onStrokeEnd={handleStrokeEnd} // Use the newly defined function
+      />
     </div>
   );
 };
