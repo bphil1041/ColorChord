38de352a706edc10d3f30acd18a482d703942a05
bphil1041 <benjamin.r.phillips1041@gmail.com>
2024-10-15 22:49:20 -0500

    Update Toolbar.js

--- colorchord/src/components/Toolbar.js
+++ colorchord/src/components/Toolbar.js
@@ -43,28 +43,34 @@ const Toolbar = ({ setSelectedColor, selectedColor, selectedBrushSize, setSelect
 
     return (
         <div style={{ position: 'fixed', top: '10px', left: '10px', zIndex: 1000 }}>
-            <div>
-                <label>Brush Size: {selectedBrushSize}px</label>
-                <input
-                    type="range"
-                    min="1"
-                    max="250"
-                    value={selectedBrushSize}
-                    onChange={handleBrushSizeChange}
-                />
+            <div style={{ display: 'flex', alignItems: 'center' }}>
+                <div style={{ marginRight: '20px' }}>
+                    <label>Brush Size: {selectedBrushSize}px</label>
+                    <input
+                        type="range"
+                        min="1"
+                        max="250"
+                        value={selectedBrushSize}
+                        onChange={handleBrushSizeChange}
+                    />
+                </div>
+                <p style={{ marginRight: '20px' }}>Shift: Octave Up</p>
+                <p>Space Bar: Octave Down</p>
             </div>
-            <button style={buttonStyle('red')} onClick={() => setSelectedColor('red')}>Red (a)</button>
-            <button style={buttonStyle('orangered')} onClick={() => setSelectedColor('orangered')}>Red-Orange (w)</button>
-            <button style={buttonStyle('orange')} onClick={() => setSelectedColor('orange')}>Orange (s)</button>
-            <button style={buttonStyle('darkorange')} onClick={() => setSelectedColor('darkorange')}>Yellow-Orange (e)</button>
-            <button style={buttonStyle('yellow')} onClick={() => setSelectedColor('yellow')}>Yellow (d)</button>
-            <button style={buttonStyle('yellowgreen')} onClick={() => setSelectedColor('yellowgreen')}>Yellow-Green (r)</button>
-            <button style={buttonStyle('green')} onClick={() => setSelectedColor('green')}>Green (f)</button>
-            <button style={buttonStyle('cyan')} onClick={() => setSelectedColor('cyan')}>Blue-Green (t)</button>
-            <button style={buttonStyle('blue')} onClick={() => setSelectedColor('blue')}>Blue (g)</button>
-            <button style={buttonStyle('indigo')} onClick={() => setSelectedColor('indigo')}>Blue-Violet (y)</button>
-            <button style={buttonStyle('purple')} onClick={() => setSelectedColor('purple')}>Violet (h)</button>
-            <button style={buttonStyle('magenta')} onClick={() => setSelectedColor('magenta')}>Red-Violet (u)</button>
+            <div>
+                <button style={buttonStyle('red')} onClick={() => setSelectedColor('red')}>Red (a)</button>
+                <button style={buttonStyle('orangered')} onClick={() => setSelectedColor('orangered')}>Red-Orange (w)</button>
+                <button style={buttonStyle('orange')} onClick={() => setSelectedColor('orange')}>Orange (s)</button>
+                <button style={buttonStyle('darkorange')} onClick={() => setSelectedColor('darkorange')}>Yellow-Orange (e)</button>
+                <button style={buttonStyle('yellow')} onClick={() => setSelectedColor('yellow')}>Yellow (d)</button>
+                <button style={buttonStyle('yellowgreen')} onClick={() => setSelectedColor('yellowgreen')}>Yellow-Green (r)</button>
+                <button style={buttonStyle('green')} onClick={() => setSelectedColor('green')}>Green (f)</button>
+                <button style={buttonStyle('cyan')} onClick={() => setSelectedColor('cyan')}>Blue-Green (t)</button>
+                <button style={buttonStyle('blue')} onClick={() => setSelectedColor('blue')}>Blue (g)</button>
+                <button style={buttonStyle('indigo')} onClick={() => setSelectedColor('indigo')}>Blue-Violet (y)</button>
+                <button style={buttonStyle('purple')} onClick={() => setSelectedColor('purple')}>Violet (h)</button>
+                <button style={buttonStyle('magenta')} onClick={() => setSelectedColor('magenta')}>Red-Violet (u)</button>
+            </div>
         </div>
     );
 };
