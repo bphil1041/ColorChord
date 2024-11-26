3ded1f6a126c71121346fe508ae9809e47526a67
bphil1041 <benjamin.r.phillips1041@gmail.com>
2024-11-26 17:07:41 -0600

    Update Toolbar.js
    update color buttons to also include pitch

--- colorchord/src/components/Toolbar.js
+++ colorchord/src/components/Toolbar.js
@@ -1,93 +1,120 @@
-import React, { useEffect, useState } from 'react';
-import './Toolbar.css';
+import React, { useEffect, useState } from "react";
+import "./Toolbar.css";
 
-const Toolbar = ({ setSelectedColor, selectedColor, selectedBrushSize, setSelectedBrushSize }) => {
-    const [isVisible, setIsVisible] = useState(true);
-    const [isFadingOut, setIsFadingOut] = useState(false);
-    const [isFadingIn, setIsFadingIn] = useState(false);
-    const keyToColor = {
-        a: 'red',
-        w: 'orangered',
-        s: 'orange',
-        e: 'darkorange',
-        d: 'yellow',
-        r: 'yellowgreen',
-        f: 'green',
-        t: 'cyan',
-        g: 'blue',
-        y: 'indigo',
-        h: 'purple',
-        u: 'magenta'
+const Toolbar = ({
+  setSelectedColor,
+  selectedColor,
+  selectedBrushSize,
+  setSelectedBrushSize,
+}) => {
+  const [isVisible, setIsVisible] = useState(true);
+  const [isFadingOut, setIsFadingOut] = useState(false);
+  const [isFadingIn, setIsFadingIn] = useState(false);
+  const keyToColor = {
+    a: "red",
+    w: "orangered",
+    s: "orange",
+    e: "darkorange",
+    d: "yellow",
+    r: "yellowgreen",
+    f: "green",
+    t: "cyan",
+    g: "blue",
+    y: "indigo",
+    h: "purple",
+    u: "magenta",
+  };
+
+  const colorToPitch = {
+    red: "C",
+    orangered: "C#",
+    orange: "D",
+    darkorange: "D#",
+    yellow: "E",
+    yellowgreen: "F",
+    green: "F#",
+    cyan: "G",
+    blue: "G#",
+    indigo: "A",
+    purple: "A#",
+    magenta: "B",
+  };
+
+  useEffect(() => {
+    const handleKeyDown = (e) => {
+      if (e.key === "Tab") {
+        e.preventDefault();
+        if (isVisible) {
+          setIsFadingOut(true);
+          setTimeout(() => {
+            setIsVisible(false);
+            setIsFadingOut(false);
+          }, 300);
+        } else {
+          setIsVisible(true);
+          setIsFadingIn(true);
+          setTimeout(() => {
+            setIsFadingIn(false);
+          }, 300);
+        }
+      }
+
+      const color = keyToColor[e.key.toLowerCase()];
+      if (color) {
+        setSelectedColor(color);
+      }
     };
 
-    useEffect(() => {
-        const handleKeyDown = (e) => {
-            // Prevent the default Tab behavior to avoid shifting focus
-            if (e.key === 'Tab') {
-                e.preventDefault();
-                if (isVisible) {
-                    setIsFadingOut(true);
-                    setTimeout(() => {
-                        setIsVisible(false);
-                        setIsFadingOut(false);
-                    }, 300); // Match this duration with the CSS transition duration
-                } else {
-                    setIsVisible(true);
-                    setIsFadingIn(true);
-                    setTimeout(() => {
-                        setIsFadingIn(false);
-                    }, 300); // Match this duration with the CSS transition duration
-                }
-            }
-
-            const color = keyToColor[e.key.toLowerCase()];
-            if (color) {
-                setSelectedColor(color);
-            }
-        };
-        
-        window.addEventListener('keydown', handleKeyDown);
-        return () => {
-            window.removeEventListener('keydown', handleKeyDown);
-        };
-    }, [setSelectedColor, isVisible]);
-
-    const handleBrushSizeChange = (e) => {
-        setSelectedBrushSize(Number(e.target.value));
+    window.addEventListener("keydown", handleKeyDown);
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown);
     };
+  }, [setSelectedColor, isVisible]);
 
-    return (
-        <div
-            className={`toolbar-container ${isFadingOut ? 'fade-out' : ''} ${isFadingIn ? 'fade-in' : ''}`}
-            style={{ display: isVisible ? 'flex' : 'none' }}
-        >
-            <div className="color-buttons">
-                {Object.entries(keyToColor).map(([key, color]) => (
-                    <button
-                        key={color}
-                        className={`color-button ${selectedColor === color ? 'selected' : ''}`}
-                        style={{ backgroundColor: color }}
-                        onClick={() => setSelectedColor(color)}
-                    >
-                        {color.charAt(0).toUpperCase() + color.slice(1)} ({key})
-                    </button>
-                ))}
-            </div>
-            <div className="toolbar-header">
-                <div className="brush-size-container">
-                    <label>Brush Size: {selectedBrushSize}px</label>
-                    <input
-                        type="range"
-                        min="1"
-                        max="500"
-                        value={selectedBrushSize}
-                        onChange={handleBrushSizeChange}
-                    />
-                </div>
-                <p className="keyboard-shortcuts"><strong>Shift:</strong> Octave Up | <strong>Space Bar:</strong> Octave Down | <strong>Tab:</strong> Hide / Show Toolbar</p>
-            </div>
+  const handleBrushSizeChange = (e) => {
+    setSelectedBrushSize(Number(e.target.value));
+  };
+
+  return (
+    <div
+      className={`toolbar-container ${isFadingOut ? "fade-out" : ""} ${
+        isFadingIn ? "fade-in" : ""
+      }`}
+      style={{ display: isVisible ? "flex" : "none" }}
+    >
+      <div className="color-buttons">
+        {Object.entries(keyToColor).map(([key, color]) => (
+          <button
+            key={color}
+            className={`color-button ${
+              selectedColor === color ? "selected" : ""
+            }`}
+            style={{ backgroundColor: color }}
+            onClick={() => setSelectedColor(color)}
+          >
+            {color.charAt(0).toUpperCase() + color.slice(1)} ({key}) -{" "}
+            {colorToPitch[color]}
+          </button>
+        ))}
+      </div>
+      <div className="toolbar-header">
+        <div className="brush-size-container">
+          <label>Brush Size: {selectedBrushSize}px</label>
+          <input
+            type="range"
+            min="1"
+            max="500"
+            value={selectedBrushSize}
+            onChange={handleBrushSizeChange}
+          />
         </div>
-    );
+        <p className="keyboard-shortcuts">
+          <strong>Shift:</strong> Octave Up | <strong>Space Bar:</strong> Octave
+          Down | <strong>Tab:</strong> Hide / Show Toolbar
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Toolbar;
